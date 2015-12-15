7c5becb0edc90675a8d7c767d1a92e999a55bb15
kapidt <kacper.krakowski@gmail.com>
2015-12-15 22:20:42 +0100

    wstep do zagadnienia

---
+++ main.m
@@ -0,0 +1,29 @@
+clear all;
+close all;
+%% samolot 
+run('AC12.m');
+
+
+%% samolot
+load('ac13_14.mat');
+
+
+
+%% helikopter
+run('HE5.m');
+
+
+
+%% helikopter
+load('he6.mat');
+
+
+
+%% silnik
+run('je1_1.m');
+
+
+
+
+%% silnik
+load('je1.mat');
